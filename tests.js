d865c5cc4a3de701531b2a386b9d73d5140e83b1
Chris Cates <codeviolet@chriscates.ca>
2016-04-21 00:57:55 -0400

    Updated unit tests

--- tests.js
+++ tests.js
@@ -1,11 +1,50 @@
+//Uncache the auth module
+require.uncache = function (moduleName) {
+    // Run over the cache looking for the files
+    // loaded by the specified module name
+    require.searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+        if (cacheKey.indexOf(moduleName)>0) {
+            delete module.constructor._pathCache[cacheKey];
+        }
+    });
+};
+
+require.searchCache = function (moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function run(mod) {
+            // Go over each of the module's children and
+            // run over it
+            mod.children.forEach(function (child) {
+                run(child);
+            });
+
+            // Call the specified callback providing the
+            // found module
+            callback(mod);
+        })(mod);
+    }
+};
 //Require dependencies
 var redis = require("redis.token")()
 var auth = require("./index.js")(redis)
+require.uncache("./index.js")
 var request = require("supertest")
-
+//Initialize express
 var express = require("express")
 var app = express()
-
+//Create API calls
 app.post("/new", function(req,res) {
   redis.generate(
     {
@@ -23,41 +62,103 @@ app.get("/auth", auth("user"), function(req,res) {
   return res.status(200).send("Authorized :)")
 })
 
+app.get("/auth_admin", auth("admin"), function(req,res) {
+  return res.status(200).send("Authorized :)")
+})
+
 app.get("/free", function(req,res) {
   return res.status(200).send("This point is free...")
 })
 
 var token = ""
 
+//UNIT TEST
 describe('Redis Auth', function() {
 
-  it('Make sure server is working', function (done) {
+  describe("With returnErrors = true", function() {
+    it('Make sure server is working', function (done) {
 
-    request(app)
-      .get("/free")
-      .expect(200, "This point is free...", done)
+      request(app)
+        .get("/free")
+        .expect(200, "This point is free...", done)
+
+    })
+
+    it('Generate a token', function (done) {
+
+      request(app)
+        .post("/new")
+        .expect(200)
+        .end(function(err,res) {
+          if (err) throw err
+          token = res.body.token
+          done()
+        })
+
+    })
+
+    it('Authenticate with middleware', function (done) {
+
+      request(app)
+        .get("/auth")
+        .set("Authorization", token)
+        .expect(200, "Authorized :)", done)
+
+    })
+
+    it('Should return a 403 - No header supplied', function (done) {
+
+      request(app)
+        .get("/auth")
+        .expect(403, done)
+
+    })
+
+    it('Should return a 403 - Incorrect grant type', function (done) {
+
+      request(app)
+        .get("/auth_admin")
+        .set("Authorization", token)
+        .expect(403, done)
+
+    })
 
   })
 
-  it('Generate a token', function (done) {
+  describe("With returnErrors = false", function() {
 
-    request(app)
-      .post("/new")
-      .expect(200)
-      .end(function(err,res) {
-        if (err) throw err
-        token = res.body.token
-        done()
-      })
+    var auth2 = require("./index.js")(redis, {
+      "header": "authorization",
+      "key": "grantType",
+      "returnErrors": false
+    })
 
-  })
+    app.get("/auth2", auth2("user"), function(req,res) {
+      if (req.errorCode == 403) return res.status(req.errorCode).send("Unauthorized...")
+      return res.status(200).send("Authorized :)")
+    })
 
-  it('Authenticate with middleware', function (done) {
+    app.get("/auth2_admin", auth2("admin"), function(req,res) {
+      if (req.errorCode == 403) return res.status(req.errorCode).send("Unauthorized...")
+      return res.status(200).send("Authorized :)")
+    })
 
-    request(app)
-      .get("/auth")
-      .set("Authorization", token)
-      .expect(200, "Authorized :)", done)
+    it('Should return a 403 - No header supplied', function (done) {
+
+      request(app)
+        .get("/auth2")
+        .expect(403, done)
+
+    })
+
+    it('Should return a 403 - Incorrect grant type', function (done) {
+
+      request(app)
+        .get("/auth2_admin")
+        .set("Authorization", token)
+        .expect(403, done)
+
+    })
 
   })
 
